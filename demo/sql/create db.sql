ce0259bac04282a9bd5afd1ba6b62525430c7e66
Anyi Shi <a26shi@uwaterloo.ca>
2022-02-10 18:40:52 -0500

    update sql

---
+++ demo/sql/create db.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `job`.`jobs` (
+  `title` TEXT NOT NULL,
+  `publish_time` INT NULL,
+  `description` TEXT NULL,
+  `company` TEXT NOT NULL,
+  `locations` TEXT NULL,
+  `apply_url` TEXT NULL,
+  `from_url` VARCHAR(300) NOT NULL,
+  `basic_qualifications` TEXT NULL,
+  `team` TEXT NULL,
+  `city` TEXT NULL,
+  `job_category` TEXT NULL,
+  `job_family` TEXT NULL,
+  `job_schedule_type` TEXT NULL,
+  `preferred_qualifications` TEXT NULL,
+  `update_time` TEXT NULL,
+  `new_grad` TEXT NULL,
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `from_url_UNIQUE` (`from_url` ASC) VISIBLE,
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
