bba8891e20c23a79f6641ae96978aa28e6904bc5
y575zhu <y575zhu@uwaterloo.ca>
2022-02-25 18:36:34 -0500

    add JPA test findJobsBy_companiesOthers and findJobsBy_cityOthers

--- demo/src/test/java/com/example/demo/job/JobRepositoryTest.java
+++ demo/src/test/java/com/example/demo/job/JobRepositoryTest.java
@@ -35,9 +35,10 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class JobRepositoryTest {
 
-//    @Container
-    private static final MySQLContainer mySQLContainer= (MySQLContainer) new MySQLContainer("mysql:latest")
-        .withReuse(true);
+
+    //    @Container
+    private static final MySQLContainer mySQLContainer = (MySQLContainer) new MySQLContainer("mysql:latest")
+            .withReuse(true);
 
     @BeforeAll
     public static void setup() {
@@ -45,21 +46,21 @@ class JobRepositoryTest {
     }
 
     @DynamicPropertySource
-    public static void overrideProps(DynamicPropertyRegistry registry){
-        registry.add("spring.datasource.url",mySQLContainer::getJdbcUrl);
-        registry.add("spring.datasource.username",mySQLContainer::getUsername);
-        registry.add("spring.datasource.password",mySQLContainer::getPassword);
+    public static void overrideProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
     }
 
     @Autowired
     private JobRepository underTest;
 
-    private final Pageable pageable0=PageRequest.of(
+    private final Pageable pageable0 = PageRequest.of(
             //page starts from 0 in pageable
             0,
             GlobalConst.PAGE_SIZE_MAX_20
     );
-    private final Pageable pageable1=PageRequest.of(
+    private final Pageable pageable1 = PageRequest.of(
             //page starts from 0 in pageable
             1,
             GlobalConst.PAGE_SIZE_MAX_20
@@ -88,29 +89,95 @@ class JobRepositoryTest {
     }
 
     @Test
+    @Sql("classpath:data.sql")
     void findJobsBy_cityOthers() {
+        //expected1: contains only other cities
+        Page<Job> expected1 = underTest.findJobsBy_cityOthers(
+                new ArrayList<>(),
+                new ArrayList<>(),
+                null,
+                "",
+                GlobalConst.TOP5_CITIES,
+                pageable0
+        );
+        assertThat(expected1.getContent()).asList()
+                .hasSize(12)
+                .extracting("city")
+                .doesNotContain(
+                        "Vancouver",
+                        "Toronto",
+                        "Waterloo",
+                        "Montreal",
+                        "Ottawa"
+                );
 
+        //expected2: contains only Toronto and other cities
+        Page<Job> expected2 = underTest.findJobsBy_cityOthers(
+                new ArrayList<>(),
+                new ArrayList<>(Arrays.asList("Toronto")),
+                null,
+                "",
+                GlobalConst.TOP5_CITIES,
+                pageable0
+        );
+        assertThat(expected2.getContent()).asList()
+                .hasSize(16)
+                .extracting("city")
+                .doesNotContain(
+                        "Vancouver",
+                        "Waterloo",
+                        "Montreal",
+                        "Ottawa"
+                );
     }
 
     @Test
+    @Sql("classpath:data.sql")
     void findJobsBy_companiesOthers() {
+        //expected1: contains only other companies
+        Page<Job> expected1 = underTest.findJobsBy_companiesOthers(
+                new ArrayList<>(),
+                new ArrayList<>(),
+                null,
+                "",
+                GlobalConst.TOP5_COMPANIES,
+                pageable0
+        );
+        assertThat(expected1.getContent()).asList()
+                .hasSize(10)
+                .extracting("company")
+                .doesNotContain(
+                        "Amazon",
+                        "Google",
+                        "Shopify",
+                        "Microsoft"
+                );
+
+        //expected2: contains only Shopify and other companies
+        Page<Job> expected2 = underTest.findJobsBy_companiesOthers(
+                new ArrayList<>(Arrays.asList("Shopify")),
+                new ArrayList<>(),
+                null,
+                "",
+                GlobalConst.TOP5_COMPANIES,
+                pageable0
+        );
+        assertThat(expected2.getContent()).asList()
+                .hasSize(13)
+                .extracting("company")
+                .doesNotContain(
+                        "Amazon",
+                        "Google",
+                        "Microsoft"
+                );
     }
 
     @Test
+    @Sql("classpath:data.sql")
     void findJobsBy_companiesCityOthers() {
-        //expected0: contains only other company and other city
-        Page<Job> expected0 = underTest.findJobsBy_companiesCityOthers(
-                new ArrayList<>(),
-                new ArrayList<>(),
-                null,
-                "",
-                GlobalConst.TOP5_CITIES,
-                GlobalConst.TOP5_COMPANIES,
-                pageable0
-        );
-        //expected1: contains Google and other company and other city
+        //expected1: contains only other companies and other cities
         Page<Job> expected1 = underTest.findJobsBy_companiesCityOthers(
-                new ArrayList<>(Arrays.asList("Google")),
+                new ArrayList<>(),
                 new ArrayList<>(),
                 null,
                 "",
@@ -118,8 +185,57 @@ class JobRepositoryTest {
                 GlobalConst.TOP5_COMPANIES,
                 pageable0
         );
-        //expected2: contains other company and Toronto and other city
+        assertThat(expected1.getContent()).asList()
+                .hasSize(9);
+        assertThat(expected1.getContent()).asList()
+                .extracting("company")
+                .doesNotContain(
+                        "Amazon",
+                        "Google",
+                        "Shopify",
+                        "Microsoft"
+                );
+        assertThat(expected1.getContent()).asList()
+                .extracting("city")
+                .doesNotContain(
+                        "Vancouver",
+                        "Toronto",
+                        "Waterloo",
+                        "Montreal",
+                        "Ottawa"
+                );
+
+        //expected2: contains Shopify and other companies and other cities
         Page<Job> expected2 = underTest.findJobsBy_companiesCityOthers(
+                new ArrayList<>(Arrays.asList("Shopify")),
+                new ArrayList<>(),
+                null,
+                "",
+                GlobalConst.TOP5_CITIES,
+                GlobalConst.TOP5_COMPANIES,
+                pageable0
+        );
+        assertThat(expected2.getContent()).asList()
+                .hasSize(11);
+        assertThat(expected2.getContent()).asList()
+                .extracting("company")
+                .doesNotContain(
+                        "Amazon",
+                        "Google",
+                        "Microsoft"
+                );
+        assertThat(expected2.getContent()).asList()
+                .extracting("city")
+                .doesNotContain(
+                        "Vancouver",
+                        "Toronto",
+                        "Waterloo",
+                        "Montreal",
+                        "Ottawa"
+                );
+
+        //expected3: contains other companies and Toronto and other cities
+        Page<Job> expected3 = underTest.findJobsBy_companiesCityOthers(
                 new ArrayList<>(),
                 new ArrayList<>(Arrays.asList("Toronto")),
                 null,
@@ -128,14 +244,23 @@ class JobRepositoryTest {
                 GlobalConst.TOP5_COMPANIES,
                 pageable0
         );
-
-        List<Page<Job>> expectedL = new ArrayList<>(Arrays.asList(expected0, expected1, expected2));
-        for (int i = 0; i < expectedL.size(); i++) {
-            assertThat(expectedL.get(i).getContent()).asList().hasSize(9);
-            assertThat(expectedL.get(i).getContent()).asList()
-                    .extracting("company").doesNotContain("Amazon", "Google", "Shopify", "Microsoft");
-            assertThat(expectedL.get(i).getContent()).asList()
-                    .extracting("city").doesNotContain("Vancouver", "Toronto", "Waterloo", "Montreal", "Ottawa");
-        }
+        assertThat(expected3.getContent()).asList()
+                .hasSize(10);
+        assertThat(expected3.getContent()).asList()
+                .extracting("company")
+                .doesNotContain(
+                        "Amazon",
+                        "Google",
+                        "Shopify",
+                        "Microsoft"
+                );
+        assertThat(expected3.getContent()).asList()
+                .extracting("city")
+                .doesNotContain(
+                        "Vancouver",
+                        "Waterloo",
+                        "Montreal",
+                        "Ottawa"
+                );
     }
 }
