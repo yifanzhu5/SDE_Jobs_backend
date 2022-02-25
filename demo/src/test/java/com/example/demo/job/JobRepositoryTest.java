ce4766672b8ecfb1b12bac9df572c075df818652
Anyi Shi <a26shi@uwaterloo.ca>
2022-02-25 14:33:58 -0500

    update JPAtest framework

--- demo/src/test/java/com/example/demo/job/JobRepositoryTest.java
+++ demo/src/test/java/com/example/demo/job/JobRepositoryTest.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.data.domain.Page;
@@ -26,43 +28,49 @@ import java.util.List;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
-@Testcontainers
+//@Testcontainers
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@TestPropertySource(properties = {
-        "spring.datasource.url=jdbc:mysql://localhost:3306/jobtest?useSSL=false"
-})
 class JobRepositoryTest {
 
-//    @BeforeAll
-//    static void init() {
-//        new MySQLContainer("mysql")
-//                .withDatabaseName("jobtest")
-//                .withUsername("root")
-//                .withPassword("65111")
-//                .start();
-//    }
 
-    @Container
-    MySQLContainer mySQLContainer=new MySQLContainer("mysql:latest")
-        .withDatabaseName("jobtest")
-        .withUsername("root")
-        .withPassword("65111");
+//    @Container
+    private static MySQLContainer mySQLContainer= (MySQLContainer) new MySQLContainer("mysql:latest")
+        .withReuse(true);
+
+    @BeforeAll
+    public static void setup() {
+        mySQLContainer.start();
+    }
+    @DynamicPropertySource
+    public static void overrideProps(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url",mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username",mySQLContainer::getUsername);
+        registry.add("spring.datasource.password",mySQLContainer::getPassword);
+    }
 
     @Autowired
     private JobRepository underTest;
 
-    private final Pageable pageable=PageRequest.of(
+    private final Pageable pageable0=PageRequest.of(
             //page starts from 0 in pageable
             0,
+            GlobalConst.PAGE_SIZE_MAX_20
+    );
+
+    private final Pageable pageable1=PageRequest.of(
+            //page starts from 0 in pageable
+            1,
             GlobalConst.PAGE_SIZE_MAX_20
     );
 
     @Test
     @Sql("/data.sql")
     void findJobsBy() {
-        Page<Job> expected=underTest.findJobsBy(new ArrayList<>(),new ArrayList<>(),null,"",pageable);
-        assertThat(expected.getContent()).asList().hasSize(20);
+        Page<Job> expected0=underTest.findJobsBy(new ArrayList<>(),new ArrayList<>(),null,"",pageable0);
+        assertThat(expected0.getContent()).asList().hasSize(20);
+        Page<Job> expected1=underTest.findJobsBy(new ArrayList<>(),new ArrayList<>(),null,"",pageable1);
+        assertThat(expected1.getContent()).asList().hasSize(3);
     }
 
     @Test
